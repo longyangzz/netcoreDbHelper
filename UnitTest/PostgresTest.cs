640ccb8c24e5722ca9761737891db258c2ad4d25
longyangzz <longyangzz@outlook.com>
2024-09-04 10:16:34 +0800

    fix: 插入语句 多线程测试

--- UnitTest/PostgresTest.cs
+++ UnitTest/PostgresTest.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.Data.Common;
+using System.Threading.Tasks;
 
 namespace UnitTest
 {
@@ -13,7 +14,7 @@ namespace UnitTest
         public PostgresTest()
         {
             DbUtils.RegisterDbProvider("postgres", "Npgsql.NpgsqlFactory, Npgsql");
-            DbUtils.AddDataSource("postgres_test", "postgres", "Host=localhost;Database=test;User Id=postgres;Password=postgres;");
+            DbUtils.AddDataSource("postgres_test", "postgres", "Host=111.198.29.34:5444;Database=mxpt_business_databases;User Id=postgres;Password=Mxpt@2024#1;");
         }
 
         [Test]
@@ -30,9 +31,26 @@ namespace UnitTest
         [Test]
         public void Order2_InsertRecord()
         {
-            string sql = "Insert Into test (id, name, age, address) Values(1, 'wangxm', 18, '河北保定')";
-            int result = DbUtils.ExecuteNonQuery("postgres_test", sql);
-            Assert.IsTrue(result == 1);
+            
+
+            //多线程并发调用sql查询
+            int threadCount = 5;
+            var tasks = new Task[threadCount];
+            for (int index = 0; index < threadCount; index++)
+            {
+                int i = index;
+                tasks[index] = Task.Run(() =>
+                {
+
+                    string sql = String.Format("Insert Into test (id, name, age, address) Values({0}, 'wangxm', 18, '河北保定')", i + 1);
+                    int result = DbUtils.ExecuteNonQuery("postgres_test", sql);
+                    Assert.IsTrue(result == 1);
+
+                });
+
+            }
+            // 等待所有任务完成
+            Task.WhenAll(tasks).Wait();
         }
 
         [Test]
@@ -53,13 +71,30 @@ namespace UnitTest
         [Test]
         public void Order4_QueryRecord()
         {
-            string sql = "Select * From test Where id=1";
-            DbDataReader dr = DbUtils.ExecuteDataReader("postgres_test", sql);
-            Assert.IsTrue(dr.HasRows);
-            Assert.IsTrue(dr.Read());
-            string name = dr.GetString(1);
-            Assert.AreEqual(name, "wangxm");
-            dr.Close();
+            //多线程并发调用sql查询
+            int threadCount = 5;
+            var tasks = new Task[threadCount];
+            for (int index = 0; index < threadCount; index++)
+            {
+                int i = index;
+                tasks[index] = Task.Run(() =>
+                {
+
+                    string sql = "Select * From test Where id=1";
+                    DbDataReader dr = DbUtils.ExecuteDataReader("postgres_test", sql);
+                    Assert.IsTrue(dr.HasRows);
+                    Assert.IsTrue(dr.Read());
+                    string name = dr.GetString(1);
+                    Assert.AreEqual(name, "wangxm");
+                    dr.Close();
+
+                });
+
+            }
+            // 等待所有任务完成
+            Task.WhenAll(tasks).Wait();
+
+            
         }
 
         [Test]
